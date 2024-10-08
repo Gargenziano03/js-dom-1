c8da68f0bc3c6d06f0ea4fd813184e39da779108
Gargenziano03 <gargenziano03@gmail.com>
2024-10-08 15:08:24 +0200

    add button + img

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('ciao');
+/*Consegna
+Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+BONUS
+Facciamo accendi e spegni:
+Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+E così via...
+Per fare questo bonus potremmo aver bisogno del metodo string.includes()
+*/
+
+//FASE 1: Seleziono gli elementi della DOM
+const buttonEl = document.querySelector('button');
+
+//FASE 2: Colleghiamo un element list al pulsante 
+function imput(){
+    console.log('testo cliccato');
+};
+
+buttonEl.addEventListener('click', imput);
